cdce7445a5a41b7f5f292b4616c578b9cdecbc2c
Seddor Ephraim <seddorephraim7@gmail.com>
2021-09-15 03:20:43 +0100

    Add translation keys to both commons.php

--- resources/lang/en/commons.php
+++ resources/lang/en/commons.php
@@ -1,5 +1,194 @@
 <?php
 
 return [
-    'hello' =>'Hellow World',
+    
+    /*
+    |--------------------------------------------------------------------------
+    | Common actions performed on resources.
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used for various / commons actions that
+    | can be performed on resources.
+    |
+    */
+
+    'actions'       => [
+        'on_entity'                 => ':Action :Entity',
+
+        'new'                       => 'New|New|New',
+        'create'                    => 'Create',
+        'create_a_new'              => 'Create New|Create New|Create New',
+        'create_new'                => 'Create New|Create New|Create New',
+        'save'                      => 'Save',
+        'register'                  => 'Register',
+
+        'read'                      => 'Read',
+
+        'show'                      => 'Show',
+        'show_infos'                => 'Show Infos',
+        'show_details'              => 'Show Details',
+        'show_deleted'              => 'Show Deleted|Show Deleted',
+        'show_entity_infos'         => 'Show :Entity Infos',
+        'show_entity_details'       => 'Show :Entity Details',
+        'show_deleted_entity'       => 'Show :Entity Deleted|Show :Entity Deleted',
+        
+        'edit'                      => 'Edit',
+        'update'                    => 'Update',
+        'delete'                    => 'Delete',
+        'delete_permanently'        => 'Delete Permanently',
+        'restore'                   => 'Restore',
+        'back'                      => 'Back',
+        'back_to'                   => 'Back to :Destination',
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Common messages that can be displayed to users.
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used for various / commons messages that
+    | can be dispalyed to users.
+    |
+    */
+
+    'messages'      => [
+        'successfully'      => [
+            'created'       => [
+                'entity'    => ':Entity has been created successfully.',
+            ],
+
+            'saved'         => [
+                'entity'    => ':Entity has been saved successfully.',
+            ],
+            
+            'retrieved'     => [
+                'entity'    => 'Retrieved :Entity successfully.',
+                'records'   => 'Retrieved records successfully.',
+                'searched'  => 'Retrieved searched records successfully.',
+                'deleted'   => 'Retrieved deleted records successfully.',
+            ],
+
+            'updated'       => [
+                'entity'    => ':Entity has been updated successfully.',
+            ],
+
+            'deleted'       => [
+                'entity'                => ':Entity has been deleted successfully.',
+                'entity_permanently'    => ':Entity has been permanently deleted successfully.',
+            ],
+
+            'restored'      => [
+                'entity'    => ':Entity has been restored successfully.',
+            ],
+
+            'login'      => [
+                'title'     => 'Login successfully.',
+                'message'   => 'Welcome, have a good session !',
+            ],
+
+            'logout'      => [
+                'title'    => 'Logout successfully.',
+                'message'  => 'Thanks for your session ! See you soon !',
+            ],
+        ],
+
+        'records'           => [
+            'not_available'     => 'No records available yet.',
+            'not_found'         => 'No records found for this request.',
+            'not_found_trashed' => 'No trashed records available.',
+        ],
+
+        'resource'          => [
+            'not_found'                     => 'The requested resource was not found.',
+            'already_exists'                => 'The resource you are trying to create already exists.',
+            'attribute_already_exists'      => 'This :attribute already exists.',
+        ],
+
+        'not_matching'      => [
+            'data'          => 'The provided data does not match our records.',
+            'attribute'     => 'The provided :attribute does not match our records.',
+            'identifiant'   => 'The provided identifiants does not match our records.',
+            'credentials'   => 'The provided credentials does not match our records.',
+            'email'         => 'The provided email does not match our records.',
+            'password'      => 'The provided password does not match our records.',
+        ],
+
+        'invalid'           => [
+            'attribute'     => 'Invalid :attribute',
+            'identifiant'   => 'Invalid identifiant',
+            'email'         => 'Invalid email address',
+            'phone_number'  => 'Invalid phone number',
+            'password'      => 'Invalid password',
+        ],
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Custom Validation Attributes
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used to swap our attribute placeholder
+    | with something more reader friendly such as "E-Mail Address" instead
+    | of "email". This simply helps us make our message more expressive.
+    |
+    */
+
+    'attributes'    => [
+        'avatar'                => 'Avatar',
+        'cover_picture'         => 'Cover Picture',
+        'background_picture'    => 'Background Picture',
+        'slug'                  => 'Slug',
+        'model'                 => 'Model',
+        'address'               => 'Address',
+        'age'                   => 'Age',
+        'available'             => 'Available',
+        'city'                  => 'City',
+        'content'               => 'Content',
+        'country'               => 'Country',
+        'current_password'      => 'Current password',
+        'date'                  => 'Date',
+        'day'                   => 'Day',
+        'description'           => 'Description',
+        'email'                 => 'Email Address',
+        'excerpt'               => 'Excerpt',
+        'first_name'            => 'First name',
+        'gender'                => 'Gender',
+        'hour'                  => 'Hour',
+        'last_name'             => 'Last name',
+        'minute'                => 'Minute',
+        'mobile'                => 'Mobile',
+        'month'                 => 'Month',
+        'name'                  => 'Name',
+        'password'              => 'Password',
+        'password_confirmation' => 'Password confirmation',
+        'confirm_password'      => 'Confirm Password',
+        'phone'                 => 'Phone',
+        'phone_number'          => 'Phone number',
+        'second'                => 'Second',
+        'sex'                   => 'Sex',
+        'size'                  => 'Size',
+        'time'                  => 'Time',
+        'title'                 => 'Title',
+        'username'              => 'Username',
+        'year'                  => 'Year',
+        'code'                  => 'Code',
+        'created_at'            => 'Created at',
+        'creation_date'         => 'Creation date',
+        'updated_at'            => 'Updated at',
+        'update_date'           => 'Update date',
+        'modification_date'     => 'Modification date',
+        'user_profile'          => 'User Profile',
+        'profile'               => 'Profile',
+        'location'              => 'Location',
+        'bio'                   => 'Bio',
+        'identifiants'          => [
+            'all'               => 'Identifiant (Email, Phone Number or Username)',
+            'only'              => [
+                'email_or_phone'    => 'Identifiant (Email or Phone Number)',
+                'email_or_username' => 'Identifiant (Email or Username)',
+                'phone_or_username' => 'Identifiant (Phone Number or Username)',
+            ],
+        ],
+    ],
+
 ];
--- resources/lang/fr/commons.php
+++ resources/lang/fr/commons.php
@@ -1,5 +1,189 @@
 <?php
 
 return [
-    'hello' =>'Salut le monde',
+    
+    /*
+    |--------------------------------------------------------------------------
+    | Common actions performed on resources.
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used for various / commons actions that
+    | can be performed on resources.
+    |
+    */
+
+    'actions'       => [
+        'on_entity'                 => ':Action :Entity',
+
+        'new'                       => 'Nouveau|Nouvel|Nouvelle',
+        'create'                    => 'Créer',
+        'create_a_new'              => 'Créer un Nouveau|Créer un Nouvel|Créer une Nouvelle',
+        'create_new'                => 'Créer Nouveau|Créer Nouvel|Créer Nouvelle',
+        'save'                      => 'Enregistrer',
+        'register'                  => 'Enregistrer',
+
+        'read'                      => 'Lire',
+
+        'show'                      => 'Afficher',
+        'show_infos'                => 'Afficher Infos',
+        'show_details'              => 'Voir Détails',
+        'show_deleted'              => 'Voir supprimé(e)|Voir supprimé(e)s',
+        'show_entity_infos'         => 'Voir :Entity Infos',
+        'show_entity_details'       => 'Voir :Entity Détails',
+        'show_deleted_entity'       => 'Voir :Entity supprimé(e)|Voir :Entity supprimé(e)s',
+        
+        'edit'                      => 'Editer',
+        'update'                    => 'Mettre à jour',
+        'delete'                    => 'Supprimer',
+        'delete_permanently'        => 'Supprimer définitivement',
+        'restore'                   => 'Restaurer',
+        'back'                      => 'Retour',
+        'back_to'                   => 'Retour à :Destination',
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Common messages that can be displayed to users.
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used for various / commons messages that
+    | can be dispalyed to users.
+    |
+    */
+
+    'messages'      => [
+        'successfully' => [
+            'created'   => [
+                'entity'    => ':Entity a été créé(e) avec succès.',
+            ],
+
+            'saved'     => [
+                'entity'    => ':Entity a été enregistré(e) avec succès.',
+            ],
+            
+            'retrieved'  => [
+                'entity'    => ':Entity a été trouvé(e) avec succès.',
+                'records'   => 'Enregistrements récupérés avec succès.',
+                'searched'  => 'Les enregistrements recherchés ont été récupérés avec succès.',
+                'deleted'   => 'Récupération des enregistrements supprimés avec succès.',
+            ],
+
+            'updated'   => [
+                'entity'    => ':Entity a été mis à jour avec succès.',
+            ],
+
+            'deleted'   => [
+                'entity'                => ':Entity a été supprimé(e) avec succès.',
+                'entity_permanently'    => ':Entity a été supprimé(e) définitivement avec succès.',
+            ],
+
+            'restored'  => [
+                'entity'    => ':Entity a été restauré(e) avec succès.',
+            ],
+
+            'login'      => [
+                'title'     => 'Connexion effectuée avec succès.',
+                'message'   => 'Bienvenue, bonne navigation !',
+            ],
+
+            'logout'      => [
+                'title'    => 'Déconnexion effectuée avec succès.',
+                'message'  => 'Merci pour votre visite ! À très bientôt !',
+            ],
+        ],
+
+        'records'   => [
+            'not_available'             => 'Aucun enregistrement disponible pour le moment.',
+            'not_found'                 => 'Aucun enregistrement trouvé pour cette demande.',
+            'not_found_trashed'         => 'Aucun enregistrement disponible dans la corbeille.',
+        ],
+
+        'resource'  => [
+            'not_found'                 => 'La ressource demandée n\'a pas été trouvée.',
+            'already_exists'            => 'La ressource que vous essayez de créer existe déjà.',   
+            'attribute_already_exists'  => 'Cet :attribute existe déjà.',
+        ],
+
+        'not_matching' => [
+            'credentials'           => 'Les identifiants fournis ne correspondent pas à nos enregistrements.',
+             'email'                => 'L\' adresse email fournie ne correspond pas à nos enregistrements.',
+             'password'             => 'Le mot de passe fourni ne correspond pas à nos enregistrements.',
+        ],
+
+        'invalid' => [
+            'attribute'     => ':Attribute invalide',
+            'identifiant'   => 'Identifiant invalide',
+            'email'         => 'Adresse email invalide',
+            'phone_number'  => 'Numéro de téléphone invalide',
+            'password'      => 'Mot de passe incorrect',
+        ],
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Custom Validation Attributes
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used to swap our attribute placeholder
+    | with something more reader friendly such as "E-Mail Address" instead
+    | of "email". This simply helps us make our message more expressive.
+    |
+    */
+
+    'attributes'    => [
+        'avatar'                => 'Avatar',
+        'cover_picture'         => 'Photo de courverture',
+        'background_picture'    => 'Photo d\'arrière plan',
+        'slug'                  => 'Slug',
+        'model'                 => 'Modele',
+        'address'               => 'Adresse',
+        'age'                   => 'Age',
+        'available'             => 'Disponible',
+        'city'                  => 'Ville',
+        'content'               => 'Contenu',
+        'country'               => 'Pays',
+        'current_password'      => 'Mot de passe actuel',
+        'date'                  => 'Date',
+        'day'                   => 'Jour',
+        'description'           => 'Description',
+        'email'                 => 'Adresse email',
+        'excerpt'               => 'Extrait',
+        'first_name'            => 'Prénom',
+        'gender'                => 'Genre',
+        'hour'                  => 'Heure',
+        'last_name'             => 'Nom',
+        'minute'                => 'Minute',
+        'mobile'                => 'Portable',
+        'month'                 => 'Mois',
+        'name'                  => 'Nom',
+        'password'              => 'Mot de passe',
+        'password_confirmation' => 'Confirmation du mot de passe',
+        'confirm_password'      => 'Confirmer le mot de passe',
+        'phone'                 => 'Téléphone',
+        'phone_number'          => 'Numéro de téléphone',
+        'second'                => 'Seconde',
+        'sex'                   => 'Sexe',
+        'size'                  => 'Taille',
+        'time'                  => 'Heure',
+        'title'                 => 'Titre',
+        'username'              => 'Nom d\'utilisateur',
+        'year'                  => 'Année',
+        'code'                  => 'Code',
+        'created_at'            => 'Créé(e) à',
+        'creation_date'         => 'Date de création',
+        'updated_at'            => 'Modifié(e) à',
+        'update_date'           => 'Date de mise à jour',
+        'modification_date'     => 'Date de modification',
+        'user_profile'          => 'Profil utilisateur',
+        'profile'               => 'Profil',
+        'identifiants'          => [
+            'all'               => 'Identifiant (Adresse Email, Téléphone ou Pseudo)',
+            'only'              => [
+                'email_or_phone'    => 'Identifiant (Adresse Email ou Téléphone)',
+                'email_or_username' => 'Identifiant (Adresse Email ou Pseudo)',
+                'phone_or_username' => 'Identifiant (Téléphone ou Pseudo)',
+            ],
+        ],
+    ],
+    
 ];
